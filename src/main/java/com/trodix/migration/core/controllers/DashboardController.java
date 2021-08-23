721b21bf0a4359a084e0756658787ecede9978eb
Sébastien Vallet <sebastien.vallet89@gmail.com>
2021-08-23 23:43:49 +0200

    suppression sockjs libs + proxy angular + conf CORS via fichier

--- src/main/java/com/trodix/migration/core/controllers/DashboardController.java
+++ src/main/java/com/trodix/migration/core/controllers/DashboardController.java
@@ -2,6 +2,7 @@ package com.trodix.migration.core.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DashboardController {
@@ -14,6 +15,14 @@ public class DashboardController {
     @GetMapping("/")
     public String index() {
         return "index.html";
+    }
+
+    /**
+     * Match everything without a suffix (so not a static resource)
+     */
+    @RequestMapping("/**/{path:[^.]*}")       
+    public String proxy() {
+        return "forward:/";
     }
 
 }
